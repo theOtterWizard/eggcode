7bd4a0f247a92fb19ccc9aa62ada28256e7250c4
Taylor the Otter <12115403+theOtterWizard@users.noreply.github.com>
2023-07-08 19:47:42 +0200

    Updated the grammar and test.egc to reflect more complex grammar. Added test.py as a file to find out how lark.visitors.Interpreter works.

--- parser.py
+++ parser.py
@@ -1,3 +1,15 @@
-import lark
+from lark import Lark, Transformer
+from pathlib import Path
 
-parser = lark.Lark()
+grammar = Path('grammar.lark').read_text()
+
+parser = Lark(grammar)
+
+tree = parser.parse(Path('test.egc').read_text())
+
+#class TreeToGcodeTree(Transformer):
+    #def __init__(self):
+        #self.variables = dict()
+        #self.output = str()
+    #def variable_declaration(self, items):
+        #self.variable
