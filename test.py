24f96c06192408a8365c55c0d1eab88353f523b1
Taylor the Otter <12115403+theOtterWizard@users.noreply.github.com>
2023-07-10 19:03:09 +0200

    Implemented while loop and figured out output cleanup.

--- test.py
+++ test.py
@@ -19,6 +19,15 @@ class Inter(Interpreter):
             return str(eval(''.join([str(self._variables[child.value]) if child.type == 'ID' else child.value for child in tree.children])))
 
     def while_loop(self, tree):
+        result = list()
+        while eval(self.visit(tree.children[0])):
+            result += self.visit(tree.children[1])
+        return result
+    
+    def code_block(self, tree):
+        return self.visit_children(tree)
+
+    def func_call(self, tree):
         pass
 
     def gcode(self, tree):
@@ -32,17 +41,20 @@ class Inter(Interpreter):
     def variable_mutation(self, tree):
         var_id = tree.children[0].value
         operation = tree.children[1].value
-        val = self.visit(tree.children[2])[0] if isinstance(tree.children[2], Tree) else tree.children[2].value
+        val = self.visit(tree.children[2]) if isinstance(tree.children[2], Tree) else tree.children[2].value
         if operation == "=": self._variables[var_id] = eval(val)
         elif operation == "+=": self._variables[var_id] += eval(val)
         elif operation == "-=": self._variables[var_id] -= eval(val)
 
 print(parsed_tree.pretty())
-print(parsed_tree)
 
 i = Inter()
-res = i.visit(parsed_tree)
-print('\n')
-print(i._variables)
-print('')
-print(res)
+vis_res = i.visit(parsed_tree)
+lines = list()
+
+for i in vis_res:
+    if isinstance(i, str): lines += [i]
+    if isinstance(i, list): lines += filter(None, i)
+
+print('\nresult:')
+print(*lines, sep='\n')
